179fbf34032c7f659cdac8574f9841e38e1ec77d
girinathchickoo <girinathgiri24@gmail.com>
2021-04-23 21:28:49 +0530

    update3

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-
-
+if (screen.width>=800){
+	
+	var hamb=document.querySelector(".hamburger");
+	hamb.style.display="none";
+}else{
+	var hamb=document.querySelector(".hamburger");
+	hamb.style.display="block";
+	var ham=document.querySelector(".nav");
+	ham.classList.remove("nav")
+	ham.classList.add("hamtonav");
+}
 
 function toggle(t){
-	var ham=document.querySelector(".hamtonav");
+
 	t.childNodes[0].classList.toggle("fa-times")
+	
+	var ham=document.querySelector("#ham");
 	ham.classList.toggle("nav")
+	
    }
+
 
